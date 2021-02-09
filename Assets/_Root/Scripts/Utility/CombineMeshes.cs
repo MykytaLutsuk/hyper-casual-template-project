33c9ac1597f221e5d201ea27e647ac9058c20b48
Nikita Lutsuk <n.lutsuk@pampamstuido.com>
2021-02-09 13:31:59 +0300

    Added new utility: Combine Meshes Change Input System from old on new

---
+++ Assets/_Root/Scripts/Utility/CombineMeshes.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace _Root.Scripts.Utility
+{
+    [RequireComponent(typeof(MeshFilter))]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class CombineMeshes : MonoBehaviour
+    {
+        [SerializeField] private Material material = default;
+    
+        void Start()
+        {
+            MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+            CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+            int i = 0;
+            while (i < meshFilters.Length)
+            {
+                combine[i].mesh = meshFilters[i].sharedMesh;
+                combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+                meshFilters[i].gameObject.SetActive(false);
+
+                i++;
+            }
+            
+            transform.GetComponent<MeshFilter>().mesh = new Mesh();
+            transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+            transform.GetComponent<MeshRenderer>().material = material;
+            transform.gameObject.SetActive(true);
+        }
+    }
+}
